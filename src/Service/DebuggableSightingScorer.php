e0eef2b70b8337a0546c092b4fce16271fb91884
mrbahadoor <mrbahadoor@gmail.com>
2024-02-14 21:33:02 +0400

    Liskov class substitution

--- src/Service/DebuggableSightingScorer.php
+++ src/Service/DebuggableSightingScorer.php
@@ -4,11 +4,18 @@ namespace App\Service;
 
 use App\Entity\BigFootSighting;
 use App\Model\BigFootSightingScore;
+use App\Model\DebuggableBigFootSightingScore;
+use Doctrine\Common\Util\Debug;
 
 class DebuggableSightingScorer extends SightingScorer
 {
-    public function score(BigFootSighting $sighting): BigFootSightingScore
+    public function score(BigFootSighting $sighting): DebuggableBigFootSightingScore
     {
-        return parent::score($sighting);
+        $bfScore =  parent::score($sighting);
+
+        return new DebuggableBigFootSightingScore(
+            $bfScore->getScore(),
+            100
+        );
     }
 }
